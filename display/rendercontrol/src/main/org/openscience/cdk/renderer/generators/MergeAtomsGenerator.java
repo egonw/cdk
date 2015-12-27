5c5ae14cbd848020ab456001e2a13b68cb6bf24b
Egon Willighagen <egon.willighagen@gmail.com>
2015-12-27 15:40:42 +0100

    Rebased on CDK 1.5.13-SNAPSHOT: Maven, GeometryUtils

--- display/rendercontrol/src/main/org/openscience/cdk/renderer/generators/MergeAtomsGenerator.java
+++ display/rendercontrol/src/main/org/openscience/cdk/renderer/generators/MergeAtomsGenerator.java
@@ -18,13 +18,12 @@
  */
 package org.openscience.cdk.renderer.generators;
 
-
 import java.awt.Color;
 
 import javax.vecmath.Point2d;
 import javax.vecmath.Vector2d;
 
-import org.openscience.cdk.geometry.GeometryTools;
+import org.openscience.cdk.geometry.GeometryUtil;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.renderer.RendererModel;
@@ -61,7 +60,7 @@ public class MergeAtomsGenerator extends BasicAtomGenerator
     		Vector2d vec = new Vector2d();
     		vec.sub( p2, p1 );
 
-    		Vector2d per = GeometryTools.calculatePerpendicularUnitVector( p1, p2 );
+    		Vector2d per = GeometryUtil.calculatePerpendicularUnitVector( p1, p2 );
     		per.scale( radius );
     		Vector2d per2 = new Vector2d();
     		per2.scale( -1 ,per);
